a0f2d78f4f99be53730050b372b854798bf958fe
SapsayRoman <166627770+SapsayRoman@users.noreply.github.com>
2024-04-11 14:39:08 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('translateButton').addEventListener('click', function() {
-    var sourceText = document.getElementById('sourceText').value;
-    // Імітація перекладу (насправді потрібно реалізувати запит до сервера)
-    document.getElementById('translationResult').innerText = "Виведенний текст: " + sourceText;
-});
