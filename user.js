c7f8381d9bb9544c116fb7a1736f5460c003f330
SapsayRoman <166627770+SapsayRoman@users.noreply.github.com>
2024-04-11 14:38:09 +0300

    Delete user.js

--- user.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loginButton').addEventListener('click', function() {
-    var username = document.getElementById('loginUsername').value;
-    var password = document.getElementById('loginPassword').value;
-
-    console.log("Спроба входу з:", username, password);
-    alert('Авторизація виконана! Перевірте консоль.');
-});
